a4b3a6d548090994b59f33d311fda5fbba764838
David Atkins <atki1080@gmail.com>
2020-01-25 14:13:01 -0700

    Display tasks in index

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,7 +2,7 @@
 
 class TasksController < ApplicationController
   def index
-    @tasks = ['Task 1', 'Task 2', 'Task 3']
+    @tasks = Task.all
   end
 
   def new
@@ -10,6 +10,7 @@ class TasksController < ApplicationController
   end
 
   def create
+
     task = Task.new({
       title: params[:task][:title],
       description: params[:task][:description]
